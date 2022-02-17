7477246614a980a5f645e7f13bb44791bc041681
JaraCR <99372636+JaraCR@users.noreply.github.com>
2022-02-16 19:25:46 -0600

    Update peso_lunar.py

--- peso_lunar.py
+++ peso_lunar.py
@@ -1,7 +1,6 @@
-peso_profe = 75
-
+peso_profe= 75
 i= 1
 
-while(i <= 12):
-  print("peso:",peso_profe,"año:",i,"total:",peso_profe * i * 0.165
-  i = i + 1
+while(i<= 15):
+    print(peso_profe*i*0.165)
+    i=i+1
